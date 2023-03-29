74348585eac37917bf06ad35a7c7b49072a3559f
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-29 15:46:00 +0200

    add boxes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 //collegare il bottone
 const button = document.getElementById('genera');
-  const mainWrapper = document.querySelector('.main-wrapper')
-console.log(mainWrapper)
-mainWrapper
+
+const container = document.querySelector('.container');
+console.log(container);
+
 //creo i box
 for (let i = 1; i <= 100; i++) {
 
-
   let boxes = document.createElement('div');
   boxes.classList.add('box');
+  //boxes.classList.add('d-none');
+  container.appendChild(boxes)
   console.log(boxes);
 
+  boxes.append(i)
 }
+
 
 
 
