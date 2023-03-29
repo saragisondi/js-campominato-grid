e71795884767040fa2993e6b5473c811596974e1
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-29 15:22:24 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//collegare il bottone
+const button = document.getElementById('genera');
+  const mainWrapper = document.querySelector('.main-wrapper')
+console.log(mainWrapper)
+mainWrapper
+//creo i box
+for (let i = 1; i <= 100; i++) {
+
+
+  let boxes = document.createElement('div');
+  boxes.classList.add('box');
+  console.log(boxes);
+
+}
+
+
+
