a782126f55489c76acdeb52873cf8407e906fc75
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-29 16:07:02 +0200

    add click function remove d-none

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
-//collegare il bottone
-const button = document.getElementById('genera');
 
 const container = document.querySelector('.container');
 console.log(container);
 
+
+
 //creo i box
 for (let i = 1; i <= 100; i++) {
-
   let boxes = document.createElement('div');
   boxes.classList.add('box');
-  //boxes.classList.add('d-none');
-  container.appendChild(boxes)
+  boxes.classList.add('d-none');
+  container.appendChild(boxes);
+  
   console.log(boxes);
-
+  
   boxes.append(i)
-}
 
+  //collegare il bottone
+  const button = document.getElementById('genera');
+  
+  button.addEventListener('click', function(){
+    boxes.classList.remove('d-none')
+  })
+} 
+  
 
-
+ 
 
