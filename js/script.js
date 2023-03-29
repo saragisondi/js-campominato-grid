c908d0e9aad4d8e56388fde6fc6a7a9b1dda7c2c
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-29 17:32:41 +0200

    modify event listener select

--- js/script.js
+++ js/script.js
@@ -1,34 +1,40 @@
 
 const container = document.querySelector('.container');
 console.log(container);
+ //collegare il bottone
+  const button = document.getElementById('levels');
 
+const select = document.querySelector('#levels');
 
+select.addEventListener('change', function() {
+  console.log(this.value);
 
-//creo i box
-for (let i = 1; i <= 100; i++) {
-  let boxes = document.createElement('div');
-  boxes.classList.add('box');
-  boxes.classList.add('d-none');
-  container.appendChild(boxes);
+  // eliminare tutti gli elementi con classe .box
+  container.innerHTML = '';
+
+  for (let i = 1; i <= this.value; i++) {
+    let boxes = document.createElement('div');
+    boxes.classList.add('box');
+    // boxes.classList.add('d-none');
+    container.appendChild(boxes);
+    
+    console.log(boxes);
+    
+    boxes.append(i)
   
-  console.log(boxes);
-  
-  boxes.append(i)
-
-  //collegare il bottone
-  const button = document.getElementById('genera');
-  
-  button.addEventListener('click', function(){
-    boxes.classList.remove('d-none')
+    boxes.addEventListener('click', function (){
+      boxes.classList.toggle('sky-blue');
+      console.log('hai cliccato sul numero' +  (i))
+      
   })
-
   
-boxes.addEventListener('click', function (){
-    boxes.classList.add('sky-blue');
-    console.log('hai cliccato sul numero' +  (i))
-})
-
-} 
+    button.addEventListener('click', function(){
+      boxes.classList.remove('d-none')
+    })
   
+  } 
+
+});
+
 
 
